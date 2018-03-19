eb7324b39058f42274e94f794bee4cd280bd368c
Marcos Serradilla Diez <marcos@sanlab.io>
2018-03-19 13:00:22 +0100

    Integración del proyecto Go para realizar las pruebas de rendimiento

---
+++ contadortest/start_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -u
+set -e
+
+_TOTAL="$1"
+_OPERACION="$2"
+_TIME=$(date +%Y%m%d%H%M%S) 
+
+for i in `seq 1 ${_TOTAL}`; do 
+    ./contadortest "${_OPERACION}" 2>> ~/alastria/logs/contadortest_"${_OPERACION}"_"${_TIME}"_"${i}".log & 
+done
+
+set +u
+set +e
