d4171bb2b6a413ca93fd95ad2982f0cb1460da1b
Daniel García Carracedo <dgarciacarracedo@deloitte.es>
2020-10-06 17:54:02 +0200

    [WIP] Bootstrap scripts
    There is a known problem with the libsodium shared objects library when
    starting the nodes. The installation is done in the alastria-node
    bootstrap script. The script has been copied to the test environment for
    a more convenient handling during the development process.

--- infrastructure/testnet/bin/bootstrap.sh
+++ infrastructure/testnet/bin/bootstrap.sh
@@ -6,13 +6,15 @@ sudo apt-get install wget git sudo netcat npm nodejs golang
 # When using docker images
 # sudo apt-get install wget git sudo netcat
 
-git clone https://github.com/alastria/alastria-node.git
-cd alastria-node/
-git checkout testnet2
+# git clone https://github.com/alastria/alastria-node.git
+# cd alastria-node/
+# git checkout testnet2
 
-cd ..
-sudo -H $PWD/alastria-node/scripts/bootstrap.sh
-rm -rf alastria-node
+# cd ..
+# sudo -H $PWD/alastria-node/scripts/bootstrap.sh
+
+sudo ./bootstrap-alastria-node.sh
+# rm -rf alastria-node
 
 echo "[*] Building fauty-node compatible geth client"
 ./bin/build_faulty_nodes.sh
